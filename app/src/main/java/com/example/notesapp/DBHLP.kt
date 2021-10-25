5c7f13c325cd72be1328fcb6692c472ebce60fba
Amaal <amaal772@gmail.com>
2021-10-25 14:16:15 +0300

    add retrive data

--- app/src/main/java/com/example/notesapp/DBHLP.kt
+++ app/src/main/java/com/example/notesapp/DBHLP.kt
@@ -1,9 +1,12 @@
 package com.example.notesapp
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+
 
 
 // her we call SQLiteOpenHelper Library context means the constructor , name "means the file name ", factory
@@ -37,5 +40,28 @@ class DBHLP (context: Context): SQLiteOpenHelper(context,"Notes.db", null,1 ) {
         // the last parameters is ContentValues
         SQLitD.insert("AddNotes ",null , CV )
     }
+
+
+    // this function will receive the name we saved it before and will return String
+    @SuppressLint("Range")
+    fun retrive():ArrayList<String>{
+        // create a variable holds array to get the data from user
+        var Not1 = arrayListOf<String>()
+        // we create variable to store the notes from AddNotes' table
+        // we call SQLitD to search from the table
+        var c: Cursor = SQLitD.query("AddNotes",null,null, null,null,null,null )
+        // her we ask the object c to search from the first column
+       if ( c.moveToFirst()){
+           // we add the note in array list  and search to all column to find the note you enter it
+           do {
+               Not1.add(c.getString(c.getColumnIndex("Note")))
+               // will search to the next column
+           }while (c.moveToNext())
+       }
+        //will return the Not1
+        return Not1
+
+    }
+
 
 }
--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,43 +1,54 @@
 package com.example.notesapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     //Declaring the UI elements.
-    lateinit var addNote:EditText
-    lateinit var btnsub:Button
-    lateinit var Notes:ArrayList<String>
+    lateinit var addNote: EditText
+    lateinit var btnsub: Button
+    lateinit var Notes: ArrayList<String>
     lateinit var myadap: RecyclerView
-    var note=""
-
+    var note = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //Initialising the UI elements and use it
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // create an object for DBHlp so we can access to AddNotes function
+        var DB = DBHLP(this)
         addNote = findViewById(R.id.edtext)
         btnsub = findViewById(R.id.b1)
         myadap = findViewById(R.id.rvmain)
-        Notes = arrayListOf()
-        myadap.adapter=myadap(Notes)
-        myadap.layoutManager=LinearLayoutManager(this)
+        // show the data in database
+        //  Retrieving the data from database and store it in array (Notes)
+        Notes = DB.retrive()
+       // defined the adapter
+        myadap.adapter = myadap(Notes)
+        myadap.layoutManager = LinearLayoutManager(this)
+
+
+        myadap.adapter?.notifyDataSetChanged()
         // create setOnClickListener
         btnsub.setOnClickListener {
-            note =addNote.text.toString()
-            // create an object for DBHlp so we can access to AddNotes function
-            var DB = DBHLP(applicationContext)
-            // her we can access to this function and  return Note
+            note = addNote.text.toString()
+
+            // her we can access to this function and  return Note,
+            // add  data in array (note ) and save it in database
             DB.AddNotes(note)
+            // last item added in array (note) will display in recycler view also the notes we  added before in database it will display
+            Notes.add(note)
             addNote.text.clear()
             addNote.clearFocus()
+            // update the data in recycler view
+            myadap.adapter?.notifyDataSetChanged()
+
             // create Toast message to show this data is saved
             Toast.makeText(this, "Data Saved Successfully", Toast.LENGTH_SHORT).show()
 
